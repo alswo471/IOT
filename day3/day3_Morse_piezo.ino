19675a7f1dcf67881cd70911a00bcf60e7af7380
alswo471 <alswo471@naver.com>
2022-09-14 11:27:36 +0900

    09/14

---
+++ day3/day3_Morse_piezo.ino
@@ -0,0 +1,57 @@
+int piezo = 9;
+
+char* letters[] = {
+  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+  ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+  "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+};
+
+char* numbers[] = {
+  "-----", ".----", "..---", "...--", "....-", ".....",
+  "-....", "--...", "---..", "----."
+};
+
+int dotDelay = 200;
+
+void setup() {
+  pinMode(piezo, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  char ch;
+  if (Serial.available()) {
+    ch = Serial.read();
+    if (ch >= 'a' && ch <= 'z') {
+      flashSequence(letters[ch - 'a']);
+    }
+    else if(ch >= 'A' && ch <= 'Z') {
+      flashSequence(letters[ch - 'A']);
+    }
+    else if(ch >= '0' && ch <= '9') {
+      flashSequence(numbers[ch - '0']);
+    }
+    else if(ch == ' ') {
+      delay(dotDelay * 4);
+    }
+  }
+} 
+
+void flashSequence(char* sequence) {
+  int i = 0;
+  while(sequence[i] != NULL) {
+    flashDotOrDash(sequence[i]);
+    i++;
+  }
+  delay(dotDelay * 3);
+}
+
+void flashDotOrDash(char dotOrDash) {
+  digitalWrite(piezo, HIGH);
+  if(dotOrDash == '.')
+    delay(dotDelay);
+  else
+    delay(dotDelay * 3);
+  digitalWrite(piezo, LOW);
+  delay(dotDelay);
+} 
